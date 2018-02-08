92c63e6fbf481c159c568631ab961779a31905ef
Jason Biniewski <jbiniewski@gmail.com>
2018-02-08 09:51:56 -0500

    ssave

---
+++ GoBills.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+
+answer = input("What is the greatest NFL team on planet earth?: ")
+counter = 1
+while (counter == 1):
+    if answer.lower() in ("buffalo", "bills", "buffalo bills", "buffalobills", "billsbuffalo", "bills buffalo"):
+        print("Correct!")
+        counter = 0
+    else:
+        print("Wrong! Try again.")
+        answer = input("What is the greatest NFL team on planet earth?: ")
+        counter = 1
