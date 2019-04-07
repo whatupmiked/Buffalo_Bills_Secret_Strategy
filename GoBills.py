ec4ebdf6d7c5feb34b86e4f650b2ca3f46215232
Michael Doyle <whatupmiked@gmail.com>
2019-04-07 12:02:34 +0100

    Go Bills

--- GoBills.py
+++ GoBills.py
@@ -4,7 +4,7 @@ answer = input("What is the greatest NFL team on planet earth?: ")
 counter = 1
 while (counter == 1):
     if answer.lower() in ("buffalo", "bills", "buffalo bills", "buffalobills", "billsbuffalo", "bills buffalo"):
-        print("Correct!")
+        print("Correct! Go Bills!")
         counter = 0
     else:
         print("Wrong! Try again.")
